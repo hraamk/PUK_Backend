e9c315793e9a732cd889ff8a8798769c892d5242
Jeevash <Jeevash@gmail.com>
2024-11-05 16:09:20 +0530

    Add CORS configuration to Express app

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = 'mongodb+srv://user:user@spaces.pkccu.mongodb.net/?retryWrites=true&w=majority&appName=Spaces'
+const url = 'mongodb+srv://user:user@spaces..mongodb.net/?retryWrites=true&w=majority&appName=Spaces'
+const cors = require('cors')
 
 const app = express();
+
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:3000', // Assuming your React app runs on port 3000
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Enable credentials (cookies, authorization headers, etc)
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +24,7 @@ const con = mongoose.connection;
 
 con.on('open', function(){
     console.log('connected...')
-
 })
-
 
 const spaceRouter = require('./routes/spaces')
 app.use('/spaces', spaceRouter)
